7c4ae10bffb94eeab546d1764bcb6f2e400223de
xuyang <1824650783@qq.com>
2019-11-05 01:47:20 +0800

    考试项目--后端管理系统

--- src/main/java/com/xuyang/springboot/exam/ExamApplication.java
+++ src/main/java/com/xuyang/springboot/exam/ExamApplication.java
@@ -2,8 +2,10 @@ package com.xuyang.springboot.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableCaching
 @EnableWebMvc
 @SpringBootApplication
 public class ExamApplication {
